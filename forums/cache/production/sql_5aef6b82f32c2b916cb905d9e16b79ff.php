3e9000ea3da16fab038925a8aa9b019efd7c01b9
The Salt Institute <76884919+TheSaltInstitute@users.noreply.github.com>
2024-02-08 16:59:20 +0100

    Add files via upload

---
+++ forums/cache/production/sql_5aef6b82f32c2b916cb905d9e16b79ff.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+1672590846
+SELECT forum_id FROM phpbb2_forums WHERE forum_options & 2 <> 0 LIMIT 1
+6
+a:0:{}
---
+++ forums/cache/production/sql_7879b42d3be9177637a34ebb1d3f0b52.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+1671483671
+SELECT forum_id, forum_name, parent_id, forum_type, left_id, right_id FROM phpbb2_forums ORDER BY left_id ASC
+863
+a:5:{i:0;a:6:{s:8:"forum_id";s:1:"4";s:10:"forum_name";s:17:"Research Requests";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"1";s:8:"right_id";s:1:"2";}i:1;a:6:{s:8:"forum_id";s:1:"3";s:10:"forum_name";s:18:"General Discussion";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"3";s:8:"right_id";s:1:"4";}i:2;a:6:{s:8:"forum_id";s:1:"6";s:10:"forum_name";s:16:"Library Research";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"5";s:8:"right_id";s:1:"6";}i:3;a:6:{s:8:"forum_id";s:1:"7";s:10:"forum_name";s:19:"Statement Follow-Up";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"7";s:8:"right_id";s:1:"8";}i:4;a:6:{s:8:"forum_id";s:1:"8";s:10:"forum_name";s:16:"Artefact Queries";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"9";s:8:"right_id";s:2:"10";}}
